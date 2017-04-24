763ad1f83bfe09837d3a74e3ef39a19646da5a0a
Dariush Alipour <drsh.alipour@gmail.com>
2017-04-24 11:05:30 +0430

    launched first working version

---
+++ index.js
@@ -0,0 +1,7 @@
+const Event = require('./src/event.class.js').Event;
+const OneDate = require('./src/onedate.class.js').OneDate;
+const GregorianAdapter = require('./src/gregorian.adapter.js').Adapter;
+
+exports.Event = Event;
+exports.OneDate = OneDate;
+exports.GregorianAdapter = GregorianAdapter;
